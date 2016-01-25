aca25643c10d2aa4f9881f4c0ea656f92a233267
Tray Harrington <traylon.harrington@gmail.com>
2016-01-25 14:40:54 -0600

    Cake

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -2,4 +2,12 @@
 # amount, with some indication of the truncating (a "..." by default).
 
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+
+  if long_str.length > num_characters
+    return long_str[0...num_characters] + " " + truncated_indicator
+  else 
+    return long_str
+  end
+
 end
+

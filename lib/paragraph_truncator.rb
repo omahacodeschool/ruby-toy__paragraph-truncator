b4a125be38a2837da6719619715e830ea68cfba2
Ryan Newell <youcatastrophe@gmail.com>
2016-01-25 12:38:47 -0600

    Done... I think?

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -2,4 +2,11 @@
 # amount, with some indication of the truncating (a "..." by default).
 
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+  truncated_str = long_str[0, num_characters] + " " + truncated_indicator
+  if truncated_str.length - 3 > num_characters
+    truncated_str
+  else 
+    long_str
+  end
 end
+puts truncate_paragraph("This is a boring paragraph and it deserves to be truncated. I mean, really. I'm not even saying anything worth reading here.", 125)

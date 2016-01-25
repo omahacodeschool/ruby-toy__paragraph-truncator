e9ce63d400f39df0f5b5efabb9bbc5fa7a8fe25e
Beth Haubert <bethany.haubert@gmail.com>
2016-01-25 13:26:40 -0600

    poo

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -1,17 +1,16 @@
 # This method takes a long string and returns the string truncated by a given
 # amount, with some indication of the truncating (a "..." by default).
-require "pry"
 
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
 	#create an empty array
-	arr = {}
 	#figure out how to code a place holder for num_characters inside the parentheses
-	arr = long_str(%{num_characters})
-	#add the "..." to the array
-	return arr + truncated_indicator
+	if long_str.length < long_str[0..num_characters-1].length
+		arr = {}
+		arr = long_str[0..num_characters-1]
+		#add the "..." to the array
+	  return arr + " " + truncated_indicator
+	end
+	elsif long_str.length >= long_str[0..num_characters-1].length
+	  return long_str
+	end
 end
-
-binding.pry
-
-puts "end"
-

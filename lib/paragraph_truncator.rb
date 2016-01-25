bb4eec5d2565437585950bb13e6a77e860abe936
Melinda Deinert <melinda.deinert@gmail.com>
2016-01-25 13:42:46 -0600

    Edited

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -1,10 +1,11 @@
 # This method takes a long string and returns the string truncated by a given
 # amount, with some indication of the truncating (a "..." by default).
 
-def truncate_paragraph(long_str, num_characters, truncated_indicator)
-  print long_str[0..num_characters] + " " + truncated_indicator
+def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+  arr = long_str.split('')
+  if long_str.length > num_characters
+    return arr[0...num_characters].join('') + " " + truncated_indicator
+  else
+    return long_str
+  end
 end
-
-
-
-puts truncate_paragraph("Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle field of that war. We come to dedicate a portion of it, as a final resting place for those who died here, that the nation might live. This we may, in all propriety do.", 25, "...")

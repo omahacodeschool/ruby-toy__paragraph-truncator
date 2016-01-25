66a11709b2916c46c3f354d02e8d1355603a8053
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-25 15:27:46 -0600

    truncator_BETTER

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -4,9 +4,11 @@
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
   arr = long_str.to_s.split("")
   long = num_characters.to_i
-    if arr.length <= long
-      puts arr.join
+    if long == 0
+      return nil
+    elsif arr.length <= long
+      return arr.join
     else
-      puts arr[0..long].join + truncated_indicator
+      return arr[0..long-1].join + " " +truncated_indicator
     end
 end

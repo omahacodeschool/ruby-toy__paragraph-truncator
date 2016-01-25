c6092c997930c98b0eef43a06b98b0e67ac2aa62
Michael Meier <mmeier61@yahoo.com>
2016-01-25 13:01:13 -0600

    Done

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -2,4 +2,10 @@
 # amount, with some indication of the truncating (a "..." by default).
 
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+  num_characters =num_characters - 1
+  if long_str.length > num_characters
+    return "#{long_str[0..num_characters]} #{truncated_indicator}"
+  else
+    return long_str
+  end
 end

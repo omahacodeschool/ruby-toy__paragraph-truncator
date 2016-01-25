3dde24bb8c1b6a5e925b070f22088d75fdecb4e9
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-25 13:28:38 -0600

    fin

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -2,4 +2,11 @@
 # amount, with some indication of the truncating (a "..." by default).
 
 def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+arr=long_str.split("") # split string into array
+goal_trunc = arr.length-num_characters #get length of array minus how many letters to leave displaying
+arr.slice!(num_characters..arr.length) #starting from indicating position slice off until the end of the array  
+puts arr.join + truncated_indicator #convert arr back to str and add indicator
 end
+
+
+

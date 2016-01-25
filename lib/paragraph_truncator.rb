8a662fc150e4e146007268a98df5fca6e854fb5b
Tyler <tfisher322@gmail.com>
2016-01-25 14:45:18 -0600

    truncate

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -5,12 +5,12 @@ def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
   p = long_str 
   n = num_characters
   ti = truncated_indicator
-  if n > p.length
-      p
-  else
-  ps = p[0, n]
-  ps + " " + ti
-end
+    if n > p.length
+        p
+    else
+      ps = p[0, n]
+      ps + " " + ti
+  end
 end
 
 puts truncate_paragraph("when I was young boy my father took me into the city to see a marching band ", 10, "...")

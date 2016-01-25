63aed38d5845c77f4ea69732b749de5b46128b6a
Chris Foral <chris.foral@gmail.com>
2016-01-25 13:57:37 -0600

    Done, again

--- lib/paragraph_truncator.rb
+++ lib/paragraph_truncator.rb
@@ -1,7 +1,9 @@
-def truncate(paragraph, int, x)
-  para_arr = paragraph.split(//)
+def truncate_paragraph(long_str, num_characters, truncated_indicator="...")
+  para_arr = long_str.chars
 
-  puts para_arr[0..int].join + x 
+  if para_arr.length <= num_characters
+    para_arr.join
+  else
+    para_arr[0...num_characters].join + " " + truncated_indicator
+  end
 end
-
-truncate("Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle field of that war. We come to dedicate a portion of it, as a final resting place for those who died here, that the nation might live. This we may, in all propriety do.", 21, "(continued)")
